3a4db304fc06e895102b099a70c2bc8c1f3fea86
Griffin Caprio <gcaprio@1530technologies.com>
2009-01-06 18:33:04 +0000

    Adding new base test class for mocking test cases.

---
+++ test/Spring/Spring.Threading.Tests/BaseMockTestCase.cs
@@ -0,0 +1,20 @@
+﻿
+using NUnit.Framework;
+using Rhino.Mocks;
+
+public class BaseMockTestCase
+{
+    protected MockRepository _repository;
+
+    [SetUp]
+    public void Init()
+    {
+        _repository = new MockRepository();
+    }
+
+    [TearDown]
+    public void Destory()
+    {
+        _repository.VerifyAll();
+    }
+}
