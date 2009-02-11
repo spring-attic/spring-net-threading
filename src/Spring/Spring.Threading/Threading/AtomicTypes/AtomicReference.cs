9c25034e5e089aaeaa86df113e9e9dd407f440b2
Andreas Doehring <adoehring@adgse.de>
2009-02-11 21:07:58 +0000

    update Spring.Core.dll to 1.2.0.20313, fix Bug in AtomicReference when call CompareAndSet on a null refer or expectedv alue is null

--- src/Spring/Spring.Threading/Threading/AtomicTypes/AtomicReference.cs
+++ src/Spring/Spring.Threading/Threading/AtomicTypes/AtomicReference.cs
@@ -96,6 +96,19 @@ namespace Spring.Threading.AtomicTypes {
         /// </returns>
         public bool CompareAndSet(T expectedValue, T newValue) {
             lock(this) {
+                // it is not possible to call _reference == expected value for a generic. So we have to handle
+                // null if T is not a value type because we can not call _reference.Equals() if _reference is null
+                if (!typeof(T).IsValueType) {
+                    // if T is not a value type handle null 
+                    if (_reference == null) {
+                        if (expectedValue == null) {
+                            _reference = newValue;
+                            return true;
+                        }
+                        return false;
+                    }
+                }
+
                 if(_reference.Equals(expectedValue)) {
                     _reference = newValue;
                     return true;
