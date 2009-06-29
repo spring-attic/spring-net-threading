a72b2893de8f56c2423d66950478ea9ca643cf06
Kenneth Xu <kxu@bn.com>
2009-06-29 03:34:07 +0000

    Additional AAA test support.

--- test/Spring/Spring.Threading.Tests/Mockery.cs
+++ test/Spring/Spring.Threading.Tests/Mockery.cs
@@ -11,6 +11,18 @@ namespace Spring
             return MakeAaaMock(m=>m.PartialMock<T>(argumentsForConstructor));
         }
 
+        public static T GenerateMultiPartialMock<T>(params Type[] extraTypes)
+            where T : class
+        {
+            return MakeAaaMock(m => m.PartialMultiMock<T>(extraTypes));
+        }
+
+        public static T GenerateMultiPartialMock<T>(Type[] extraTypes, params object[] argumentsForConstructor)
+            where T : class
+        {
+            return MakeAaaMock(m => m.PartialMultiMock<T>(extraTypes, argumentsForConstructor));
+        }
+
         public static T GenerateMultiMock<T>(Type[] extraTypes, params object[] argumentForConstructor)
         {
             return MakeAaaMock(m=>m.DynamicMultiMock<T>(extraTypes, argumentForConstructor));
