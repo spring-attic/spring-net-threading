e01fb7c087aaf813ebca4b89f78701c9b51f16b0
Kenneth Xu <kxu@bn.com>
2010-02-06 15:11:58 +0000

    Add missing files in previous commit.

---
+++ create-distribution.bat
@@ -0,0 +1,4 @@
+nant distribute
+nant distribute -D:solution.targetframework=net-2.0
+nant distribute -D:build.configuration=Release
+nant distribute -D:solution.targetframework=net-2.0 -D:build.configuration=Release
