61da5e5d7e1c0f77d4c9bb39b7600869dcb495cc
Spring Operator <spring-operator@users.noreply.github.com>
2019-03-21 15:33:49 -0500

    URL Cleanup
    This commit updates URLs to prefer the https protocol. Redirects are not followed to avoid accidentally expanding intentionally shortened URLs (i.e. if using a URL shortener).
    
    # Fixed URLs
    
    ## Fixed Success
    These URLs were switched to an https URL with a 2xx status. While the status was successful, your review is still recommended.
    
    * [ ] http://www.apache.org/licenses/ with 11 occurrences migrated to:
      https://www.apache.org/licenses/ ([https](https://www.apache.org/licenses/) result 200).
    * [ ] http://www.apache.org/licenses/LICENSE-2.0 with 163 occurrences migrated to:
      https://www.apache.org/licenses/LICENSE-2.0 ([https](https://www.apache.org/licenses/LICENSE-2.0) result 200).

--- test/Spring/Spring.Threading.Tests/Threading/Execution/ExecutorServiceTestFixture.cs
+++ test/Spring/Spring.Threading.Tests/Threading/Execution/ExecutorServiceTestFixture.cs
@@ -6,7 +6,7 @@
 * you may not use this file except in compliance with the License.
 * You may obtain a copy of the License at
 * 
-*      http://www.apache.org/licenses/LICENSE-2.0
+*      https://www.apache.org/licenses/LICENSE-2.0
 * 
 * Unless required by applicable law or agreed to in writing, software
 * distributed under the License is distributed on an "AS IS" BASIS,
@@ -287,7 +287,7 @@ namespace Spring.Threading.Execution
             Assert.That(e.ParamName, Is.EqualTo("call"));
         }
 
-        [Test, Description("Submit(runnable, result£©runs the runnable and sets the future result")]
+        [Test, Description("Submit(runnable, resultï¿½ï¿½runs the runnable and sets the future result")]
         public void SubmitExecutesTheRunnableAndSetFutureResult()
         {
             var future = ExecutorService.Submit(_runnable, TestData<T>.One);
@@ -297,7 +297,7 @@ namespace Spring.Threading.Execution
             JoinPool(ExecutorService);
         }
 
-        [Test, Description("Submit(action, result£©runs the action and sets the future result")]
+        [Test, Description("Submit(action, resultï¿½ï¿½runs the action and sets the future result")]
         public void SubmitExecutesTheActionAndSetFutureResult()
         {
             var future = ExecutorService.Submit(_action, TestData<T>.One);
